f73c6277504bad1850f7b57858c979bacb1b71aa
Daniel Fors <daniel.lennart.fors@gmail.com>
2020-11-14 18:32:44 +0100

    compute boys

---
+++ Assets/Scripts/Boids/BoidHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoidHelper
+{
+    const int NumViewDirections = 200;
+    public static readonly Vector3[] Directions;
+
+    static BoidHelper()
+    {
+        Directions = new Vector3[NumViewDirections];
+
+        float GoldenRatio = (1 + Mathf.Sqrt(5)) / 2;
+        float AngleIncrement = Mathf.PI * 2 * GoldenRatio;
+
+        for (int i = 0; i < NumViewDirections; i++)
+        {
+            float t = (float)i / NumViewDirections;
+            float Inclination = Mathf.Acos(1 - 2 *t);
+            float Azimuth = AngleIncrement * i;
+
+            float x = Mathf.Sin(Inclination) * Mathf.Cos(Azimuth);
+            float y = Mathf.Sin(Inclination) * Mathf.Sin(Azimuth);
+            float z = Mathf.Cos(Inclination);
+            Directions[i] = new Vector3(x, y, z);
+        }
+    }
+}
---
+++ Assets/Scripts/Boids/BoidSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Settings yao")]
+public class BoidSettings : ScriptableObject
+{
+    public float MinSpeed = 2;
+    public float MaxSpeed = 5;
+    public float PerceptionRadius = 2.5f;
+    public float AvoidanceRadius = 1;
+    public float MaxSteerForce = 3;
+
+    public float AlingWeight = 1;
+    public float CohesionWeight = 1;
+    public float SeperateWeight = 1;
+    public float WanderWeigt = 1;
+
+    public float TargetWeight = 1;
+
+    public LayerMask ObstacleMask;
+    public float BoundsRadius = 0.27f;
+    public float AvoidCollisionWeight = 10;
+    public float CollisionAvoidDistance = 5;
+
+    public float WanderJitter = .7f;
+    public float WanderRadius = 8;
+    public float WanderDistance = 5;
+}
