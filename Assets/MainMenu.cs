3f7694928fec6698ae094ea5a1afbd604ee182bd
Per Åkerström <akerstrom.perry@gmail.com>
2020-11-14 15:57:46 +0100

    player update

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button StartButton;
+    private void Start()
+    {
+        StartButton.onClick.AddListener(StartGame);
+    }
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
