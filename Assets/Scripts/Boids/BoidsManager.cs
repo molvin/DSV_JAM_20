792de4ecff9173999bea071155f26a12f98f64ae
Daniel Fors <daniel.lennart.fors@gmail.com>
2020-11-14 20:13:11 +0100

    lady

--- Assets/Scripts/Boids/BoidsManager.cs
+++ Assets/Scripts/Boids/BoidsManager.cs
@@ -70,7 +70,9 @@ public class BoidsManager : MonoBehaviour
     public static void DeleteBoid(BoidComp Boy)
     {
         Instance.Boids.Remove(Boy);
-        Boy.gameObject.AddComponent<Rigidbody>().useGravity = false;
+        Rigidbody rb = Boy.gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = Random.onUnitSphere * 3;
         Destroy(Boy);
     }
 
