b3c5b6d86f61794d136dbcc5165c9700e6574eac
RollarN <43541489+RollarN@users.noreply.github.com>
2020-11-14 16:03:35 +0100

    Objectpool ref

--- Assets/Scripts/ProjectileWeaponThings/Weapon.cs
+++ Assets/Scripts/ProjectileWeaponThings/Weapon.cs
@@ -7,14 +7,13 @@ public class Weapon : MonoBehaviour
     [SerializeField] protected ProjectileData m_projectileData;
     protected ObjectPool m_objectPool;
     protected float firingCooldownTime;
-    private void Start()
-    {
-        m_objectPool = ObjectPool.Instance;
-    }
+    private ParticleSystem MuzzleFlash;
+
     public void FireProjectile(Vector3 Dir, Vector3 Position)
     {
-        Projectile Projectile = m_objectPool.rentObject(ObjectPool.ObjectType.ProjectileVFX).GetComponent<Projectile>();
+        Projectile Projectile = ObjectPool.Instance.rentObject(ObjectPool.ObjectType.ProjectileVFX).GetComponent<Projectile>();
         Projectile.InitializeProjectile(m_projectileData, Position, Quaternion.LookRotation(Dir));
         firingCooldownTime = Time.time + m_projectileData.firingCooldown;
     }
+
 }
