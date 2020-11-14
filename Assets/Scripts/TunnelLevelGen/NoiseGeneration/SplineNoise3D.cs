74f176d964610d3e47ed8608a57b12c299024238
DanielAkesson <argilstudio@gmail.com>
2020-11-14 18:47:12 +0100

    world stuff

--- Assets/Scripts/TunnelLevelGen/NoiseGeneration/SplineNoise3D.cs
+++ Assets/Scripts/TunnelLevelGen/NoiseGeneration/SplineNoise3D.cs
@@ -5,20 +5,57 @@ using UnityEngine;
 public class SplineNoise3D
 {
     public static List<Spline> SplineLine = new List<Spline>();
+    public static List<Spline> SplineHole = new List<Spline>();
     public static float SplineNoise(Vector3 point)
     {
+        return SplineDistance(point);
+    }
+    public static float SplineDistance(Vector3 point)
+    {
+        float smaletsFound = float.MaxValue;
         //OnLine
-        for(int i=0;i<SplineLine.Count-1;i++)
+        for (int i = 0; i < SplineLine.Count - 1; i++)
         {
-            if(IsOnLine(SplineLine[i].pos, SplineLine[i+1].pos, point))
-                return LineDistance(SplineLine[i].pos, SplineLine[i + 1].pos, point);
+            if (IsOnLine(SplineLine[i].pos, SplineLine[i + 1].pos, point))
+            {
+                float value = LineDistance(SplineLine[i].pos, SplineLine[i + 1].pos, point);
+                if (value < smaletsFound)
+                    smaletsFound = value;
+            } 
         }
+        if (smaletsFound != float.MaxValue)
+            return smaletsFound;
+
         //InJoint
-        for (int i = 1; i < SplineLine.Count-1; i++)
+        for (int i = 1; i < SplineLine.Count - 1; i++)
         {
             float hypo = Vector3.Distance(point, SplineLine[i].pos);
             if (hypo < SplineLine[i].radius)
-                return (LineDistance(SplineLine[i - 1].pos, SplineLine[i].pos, point) + LineDistance(SplineLine[i].pos, SplineLine[i+1].pos, point)) / 2f;
+                return (LineDistance(SplineLine[i - 1].pos, SplineLine[i].pos, point) + LineDistance(SplineLine[i].pos, SplineLine[i + 1].pos, point)) / 2f;
+        }
+        return 100;
+    }
+    public static float HoleNoise(Vector3 point)
+    {
+        float smaletsFound = float.MaxValue;
+        //OnLine
+        for (int i = 0; i < SplineHole.Count - 1; i++)
+        {
+            if (IsOnLine(SplineHole[i].pos, SplineHole[i + 1].pos, point))
+            {
+                float value = LineDistance(SplineHole[i].pos, SplineHole[i + 1].pos, point);
+                if (value < smaletsFound)
+                    smaletsFound = value;
+            }
+        }
+        if (smaletsFound != float.MaxValue)
+            return smaletsFound;
+        //InJoint
+        for (int i = 1; i < SplineHole.Count - 1; i++)
+        {
+            float hypo = Vector3.Distance(point, SplineHole[i].pos);
+            if (hypo < SplineHole[i].radius)
+                return (LineDistance(SplineHole[i - 1].pos, SplineHole[i].pos, point) + LineDistance(SplineHole[i].pos, SplineHole[i + 1].pos, point)) / 2f;
         }
         return 100;
     }
@@ -34,19 +71,64 @@ public class SplineNoise3D
         Vector3 BA = lineB - lineA;
         return Vector3.Dot(CA, BA.normalized);
     }
+    public static float distanceOnSpline(Vector3 pointC)
+    {
+        float total = 0f;
+        for (int i = 0; i < SplineLine.Count - 1; i++)
+        {
+            float maxDist = (SplineLine[i].pos - SplineLine[i + 1].pos).magnitude;
+            float dis = distanceOnLine(SplineLine[i].pos, SplineLine[i + 1].pos, pointC);
+            if (dis > maxDist)
+            {
+                total += maxDist;
+            }
+            else
+            {
+                total += dis;
+                return total;
+            }
+        }
+        return 0f;
+    }
+    public static Vector3 getPointOnSpline(Vector3 pointC)
+    {
+        for (int i = 0; i < SplineLine.Count - 1; i++)
+        {
+            if (IsOnLine(SplineLine[i].pos, SplineLine[i + 1].pos, pointC))
+            {
+                float dis = LineDistance(SplineLine[i].pos, SplineLine[i + 1].pos, pointC);
+                return SplineLine[i].pos + (SplineLine[i + 1].pos - SplineLine[i].pos).normalized * dis;
+            }
+        }
+        //InJoint
+        for (int i = 0; i < SplineLine.Count - 1; i++)
+        {
+            float maxDist = (SplineLine[i+1].pos - SplineLine[i].pos).magnitude;
+            float dis = distanceOnLine(SplineLine[i].pos, SplineLine[i + 1].pos, pointC);
+            if (dis <= maxDist)
+            {
+                return SplineLine[i].pos;
+            }
+        }
+        return SplineLine[SplineLine.Count-1].pos;
+    }
     public static bool IsOnLine(Vector3 lineA, Vector3 lineB, Vector3 pointC)
     {
         float alongLine = distanceOnLine(lineA, lineB, pointC);
         Vector3 BA = lineB - lineA;
         return alongLine > 0f && alongLine <= BA.magnitude;
     }
-
     //Spline stuff
-    public static void AddSplineSegment(Vector3 pos)
+    public static void AddSplineSegment(Vector3 pos, float holeOffsetRadius)
     {
         SplineLine.Add(new Spline { pos = pos, radius = 10 });
+        //Hole!
+        float x = (Perlin3D.PerlinNoise3D(pos + Vector3.up) -0.5f) * 2 * holeOffsetRadius;
+        float y = (Perlin3D.PerlinNoise3D(pos + Vector3.right) - 0.5f) *2 * holeOffsetRadius;
+        float z = (Perlin3D.PerlinNoise3D(pos + Vector3.forward) - 0.5f) *2 * holeOffsetRadius;
+        Vector3 off = new Vector3(x, y, z);
+        SplineHole.Add(new Spline { pos = pos + off, radius = 5f });
     }
-
     public struct Spline
     {
         public Vector3 pos;
--- Assets/Scripts/TunnelLevelGen/TunnelMaker.cs
+++ Assets/Scripts/TunnelLevelGen/TunnelMaker.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class TunnelMaker : MonoBehaviour
 {
     public PointCloudManager PCM;
+    public float CaveAmount = 4f;
     private void OnDrawGizmos()
     {
         for (int i = 0; i < SplineNoise3D.SplineLine.Count - 1; i++)
         {
             Debug.DrawLine(SplineNoise3D.SplineLine[i].pos, SplineNoise3D.SplineLine[i + 1].pos, Color.red);
+        }
+        for (int i = 0; i < SplineNoise3D.SplineHole.Count - 1; i++)
+        {
+            Debug.DrawLine(SplineNoise3D.SplineHole[i].pos, SplineNoise3D.SplineHole[i + 1].pos, Color.green);
         }
         for (int i = 0; i < SplineNoise3D.SplineLine.Count; i++)
         {
@@ -20,27 +25,25 @@ public class TunnelMaker : MonoBehaviour
     void Start()
     {
         SplineNoise3D.SplineLine = new List<SplineNoise3D.Spline>();
-        StartCoroutine(createLevelSLowLike(20, 0.6f, 0.4f));
-        PCM.isoSurface = 10f;
     }
     public void makeSpline(int segmentCount, float sporadicFactor,float noiseScale)
     {
         Perlin3D.scale = noiseScale;
         Vector3 direction = Vector3.right;
-        Vector3 currentPos = Vector3.one * 20;
+        Vector3 currentPos = Vector3.one * 40;
         for(int i=0;i< segmentCount; i++)
         {
-            float x = Perlin3D.PerlinNoise3D(currentPos + Vector3.right);
-            float y = Perlin3D.PerlinNoise3D(currentPos + Vector3.up);
-            float z = Perlin3D.PerlinNoise3D(currentPos + Vector3.forward);
+            float x = 0.1f;
+            float y = Perlin3D.PerlinNoise3D(currentPos + Vector3.up) - 0.5f;
+            float z = Perlin3D.PerlinNoise3D(currentPos + Vector3.forward) - 0.5f;
             direction = Vector3.Slerp(direction, new Vector3(x, y, z).normalized, sporadicFactor);
             currentPos += direction.normalized * 20f;
-            SplineNoise3D.AddSplineSegment(currentPos);
+            SplineNoise3D.AddSplineSegment(currentPos, 10f);
         }    
     }
     public void addOne(Vector3 paintPoint)
     {
-        PCM.InitializeIsoSurfaceSphere(paintPoint, 0.1f, SplineNoise3D.SplineNoise);
+        PCM.InitializeIsoSurfaceSphere(paintPoint, 0.1f, SuperNoise);
     }
     public IEnumerator createLevelSLowLike(int segmentCount, float sporadicFactor, float noiseScale)
     {
@@ -51,4 +54,17 @@ public class TunnelMaker : MonoBehaviour
             yield return null;
         }
     }
+
+    public float SuperNoise(Vector3 point)
+    {
+        float holesize = 2f;
+        //distance along spline X
+        float dist = SplineNoise3D.HoleNoise(point)/holesize;
+        if (dist > 1f) dist = 1f;
+        Perlin3D.scale = 0.2f;
+        float caveWalls = SplineNoise3D.SplineNoise(point) + Perlin3D.PerlinNoise3D(point) * CaveAmount;
+        if (caveWalls < 10f) caveWalls = 0f;
+        return (caveWalls + Perlin3D.PerlinNoise3D(point) * 12f) * dist;
+    }
+
 }
