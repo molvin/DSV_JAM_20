a7769b4b0a1def8d1f00e75e927be8717631198f
Daniel Fors <daniel.lennart.fors@gmail.com>
2020-11-15 11:28:46 +0100

    audio juice

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioReverbZone ReverbZone;
+
+    private void Start()
+    {
+        Player.FlyingState.OnBoostStart += () => ReverbZone.reverbPreset = AudioReverbPreset.Psychotic;
+        Player.FlyingState.OnBoostEnd += () => ReverbZone.reverbPreset = AudioReverbPreset.Cave;
+    }
+}
