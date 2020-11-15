11fd533fac982f0c872b8b634c85f9c42cdf4d25
todorowski <43949713+todorowski@users.noreply.github.com>
2020-11-15 13:46:53 +0100

    main manu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,12 +7,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button StartButton;
+    public Button ExitButton;
     private void Start()
     {
         StartButton.onClick.AddListener(StartGame);
+        ExitButton.onClick.AddListener(ExitGame);
     }
     private void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
     }
 }
