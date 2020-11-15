8d9c94306898d9ad0bdba05e4e9ba58515f02e0f
RollarN <43541489+RollarN@users.noreply.github.com>
2020-11-15 13:59:33 +0100

    Undestructible Music

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager: MonoBehaviour
+{
+    public static MusicManager Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(this.gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
